2cb812826fff3c1710cbc7ce35983277e2f66683
eln_billie <me.billie@yahoo.com>
2023-11-17 19:48:27 +0700

    Menambahkan icon dan fungsinyas ke Main

--- app/src/main/java/com/MuhammadBillieElianJBusRS/jbus_android/LoginActivity.java
+++ app/src/main/java/com/MuhammadBillieElianJBusRS/jbus_android/LoginActivity.java
@@ -2,13 +2,42 @@ package com.MuhammadBillieElianJBusRS.jbus_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView Daftar = null;
+    private Button button = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Daftar = findViewById(R.id.Daftar);
+        button = findViewById(R.id.button);
+
+        Daftar.setOnClickListener(v->{
+            moveActivity(this, RegisterActivity.class);
+        });
+
+        button.setOnClickListener(v->
+                moveActivity(this, MainActivity.class));
+
+        getSupportActionBar().hide();
+
     }
+    private void moveActivity(Context ctx, Class<?> cls ){
+        Intent intent = new Intent(ctx, cls);
+        startActivity(intent);
+    }
+
+    private void viewToast(Context ctx, String message){
+        Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
+    }
+
 }
---
+++ app/src/main/java/com/MuhammadBillieElianJBusRS/jbus_android/MainActivity.java
@@ -0,0 +1,22 @@
+package com.MuhammadBillieElianJBusRS.jbus_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu); // Ganti 'menu_main' dengan nama file XML menu Anda
+        return true;
+    }
+}
